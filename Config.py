b37e4b916c02d6b2298e6d7d1dc39485a7d76db1
Mohammad Abir <78835417+Abirx33@users.noreply.github.com>
2021-02-25 21:23:13 +0600

    Delete Config.py

--- Config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    CLIENT_ID = os.environ.get("CLIENT_ID")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    LOG_CHAT_ID = os.environ.get("LOG_CHAT_ID")
-    MESSAGE_ID = os.environ.get("LOG_CHAT_ID")
